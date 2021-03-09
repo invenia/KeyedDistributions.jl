177f619836be3540de6945e438b126340ee5134e
Ben Cottier <ben.cottier@invenialabs.co.uk>
2021-03-09 18:07:53 +0000

    Reorganise tests
    - Increase coverage
    - Separate Distribution-only methods

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,26 +7,25 @@ using Statistics
 using Test
 
 @testset "KeyedDistributions.jl" begin
-    # Write your tests here.
+    X = rand(MersenneTwister(1234), 10, 3)
+    m = vec(mean(X; dims=1))
+    s = cov(X; dims=1)
+    d = MvNormal(m, s)
+    keys = [:a, :b, :c]
+    kd = KeyedDistribution(d, keys)
 
     @testset "Common" begin
-        X = rand(MersenneTwister(1234), 10, 5)
-        m = vec(mean(X; dims=1))
-        s = cov(X; dims=1)
-        d = MvNormal(m, s)
-        keys = [:a, :b, :c, :d, :e]
-
-        @testset for T in (KeyedDistribution, KeyedSampleable)
+        @testset "$T" for T in (KeyedDistribution, KeyedSampleable)
             kd = T(d, keys)
 
             @testset "base functions" begin
                 @test kd isa Sampleable
                 @test distribution(kd) == d
                 @test parent(kd) == d
+                @test AxisKeys.keyless(kd) == d
                 @test axiskeys(kd) == (keys, )
-                @test length(kd) == length(d) == 5
-                @test eltype(kd) == eltype(d) == Float64
-                @test isequal(kd, T(d, [:a, :b, :c, :d, :e]))
+                @test length(kd) == length(d) == 3
+                @test isequal(kd, T(d, [:a, :b, :c]))
                 @test ==(kd, T(d, keys))
             end
 
@@ -38,71 +37,63 @@ using Test
                 rng = MersenneTwister(1)
 
                 @testset "one-sample method" begin
-                    expected = [
-                        0.6048058078690228,
-                        0.5560878435408365,
-                        0.41599188102577894,
-                        0.4756226986245742,
-                        0.15366818427047801,
-                    ]
+                    expected = [0.6048058078690228, 0.5560878435408364, 0.41599188102577894]
                     observed = rand(rng, kd)
                     @test observed isa KeyedArray
                     @test isapprox(observed, expected)
                     @test isapprox(observed(:a), expected[1])
-                    # @test axisnames(observed) == (:variates,)
                     @test first(axiskeys(observed)) == first(axiskeys(kd))
                 end
 
                 @testset "multi-sample method" begin
                     expected = [
-                        0.6080151671094673,
-                        1.2415182218538203,
-                        -4.4285504138930065e-5,
-                        0.7298398256256964,
-                        0.2103467702699237,
+                        0.5333595810495181 1.0686337176543628;
+                        0.12636240168741192 -0.44724174718706566;
+                        0.579000443516295 0.914066493850531
                     ]
-                    observed = rand(rng, kd, 1)
+                    observed = rand(rng, kd, 2)
                     @test observed isa KeyedArray
                     @test isapprox(observed, expected)
-                    @test isapprox(observed(:a), [expected[1]])
-                    # @test axisnames(observed) == (:variates, :samples)
+                    @test isapprox(observed(:a), expected[1, :])
                     @test first(axiskeys(observed)) == first(axiskeys(kd))
                 end
             end
         end
     end
 
+    @testset "Non-Distribution Sampleable only" begin
+        sampler = Distributions.MultinomialSampler(3, [0.1, 0.2, 0.3])
+
+        @test axiskeys(sampler) == (Base.OneTo(3), )
+    end
+
     @testset "KeyedDistribution only" begin
         @testset "Inner keys constructor" begin
-            keys = [:a, :b]
-            m = KeyedArray([1., 2.], keys)
-            d = MvNormal(m, [1., 1.])
-            kd = KeyedDistribution(d)
+            kd2 = KeyedDistribution(MvNormal(KeyedArray(m, keys), s))
 
-            @test distribution(kd) == d
-            @test axiskeys(kd) == (keys, )
-            @test mean(kd) == m
+            @test kd2 isa Distribution
+            @test distribution(kd2) == d
+            @test axiskeys(kd2) == (keys, )
+            @test mean(kd2) == m
+        end
+
+        kd = KeyedDistribution(d, keys)
+
+        @testset "base functions" begin
+            @test kd isa Distribution
+            @test sampler(kd) == sampler(d)
+            @test eltype(kd) == eltype(d) == Float64
         end
 
         @testset "statistical functions" begin
-            X = rand(MersenneTwister(1234), 10, 5)
-            m = vec(mean(X; dims=1))
-            s = cov(X; dims=1)
-            d = MvNormal(m, s)
-            keys = [:a, :b, :c, :d, :e]
-            kd = KeyedDistribution(d, keys)
-
             @test mean(kd) isa KeyedArray{Float64, 1}
             @test parent(mean(kd)) == mean(d) == m
-            # @test axisnames(mean(kd)) == (:variates,)
 
             @test var(kd) isa KeyedArray{Float64, 1}
             @test parent(var(kd)) == var(d) == diag(s)
-            # @test axisnames(var(kd)) == (:variates,)
 
             @test cov(kd) isa KeyedArray{Float64, 2}
             @test parent(cov(kd)) == cov(d) == s
-            # @test axisnames(cov(kd)) == (:variates, :variates_)
 
             @test entropy(kd) isa Number
             @test entropy(kd) == entropy(d)
@@ -120,6 +111,7 @@ using Test
 
     @testset "Invalid keys $T" for T in (KeyedDistribution, KeyedSampleable)
         @test_throws DimensionMismatch T(MvNormal(ones(3)), ["foo"])
+        @test_throws MethodError T(MvNormal(ones(3)), (:a, :b, :c))  # because Tuple
     end
 
 end
