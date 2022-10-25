c2661d2b0e682656a6072d136febde106a5132bc
Matthew Priddin <matthew.priddin@invenialabs.co.uk>
2022-10-25 14:13:47 +0100

    Add tests for alternative argument order

--- test/runtests.jl
+++ test/runtests.jl
@@ -367,9 +367,15 @@ using Test
 
     @testset "Equality comparison with wrapped type" begin
         d = MvNormal([1.0, 2.0], [1.0, 1.0]);
+
         kd = KeyedDistribution(d, 1:length(d));
-        ks = KeyedSampleable(d, 1:length(d));
+
         @test kd == d
+        @test d == kd
+
+        ks = KeyedSampleable(d, 1:length(d));
+
         @test ks == d
+        @test d == ks
     end
 end
